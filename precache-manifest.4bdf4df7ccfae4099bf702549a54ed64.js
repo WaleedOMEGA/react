98edbf243efb8715590934f7689547deb555877e
waleedomega <waleedomega@hotmail.com>
2022-01-12 23:26:09 +0200

    Updates

---
+++ precache-manifest.4bdf4df7ccfae4099bf702549a54ed64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac52a6492bcd5561f735ebcbdfce4358",
+    "url": "/WaleedOMEGA/react/index.html"
+  },
+  {
+    "revision": "8e60d854e763c00fc3e5",
+    "url": "/WaleedOMEGA/react/static/css/main.e91ef258.chunk.css"
+  },
+  {
+    "revision": "848166be9b0e8b6d78bf",
+    "url": "/WaleedOMEGA/react/static/js/2.a5c066f1.chunk.js"
+  },
+  {
+    "revision": "8e60d854e763c00fc3e5",
+    "url": "/WaleedOMEGA/react/static/js/main.856c5cc8.chunk.js"
+  },
+  {
+    "revision": "c42de6ee2dd00ed3fa22",
+    "url": "/WaleedOMEGA/react/static/js/runtime~main.a6ec7153.js"
+  }
+]);
