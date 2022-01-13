2ecc2d08f5dd2c8029a3e26b3f9b49b4733087e4
waleedomega <waleedomega@hotmail.com>
2022-01-13 19:54:34 +0200

    meme memeapp init

---
+++ meme-generator/src/components/MemeApp.js
@@ -0,0 +1,9 @@
+const MemeApp = () => {
+	return (
+		<div>
+			{' '}
+			<h1>Hello from parent app</h1>{' '}
+		</div>
+	);
+};
+export default MemeApp;  
--- meme-generator/src/index.js
+++ meme-generator/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+const element = <h1>React Meme Application</h1>;  
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
 
