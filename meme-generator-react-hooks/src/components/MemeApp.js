d1f22e3a6f13d3ce77fb13583f84c4be2aec96b2
waleedomega <waleedomega@hotmail.com>
2022-01-17 22:02:06 +0200

    meme-generator-react-hooks complete

---
+++ meme-generator-react-hooks/src/components/MemeApp.js
@@ -0,0 +1,11 @@
+import Header from './Header';
+import MemeGenerator from './MemeGenerator ';
+const MemeApp = () => {
+	return (
+		<div>
+            <Header />
+            <MemeGenerator />
+		</div>
+	);
+};
+export default MemeApp;
