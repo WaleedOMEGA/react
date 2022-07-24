4f01116f0a17c816f06e869b7c7d7a23771b7720
Waleed OMEGA <waleedomega@hotmail.com>
2022-07-24 14:24:31 +0200

    react complete guide expense form init

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,22 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
-const ExpenseItem=(props)=> {
+import { useState } from 'react';
+
+const ExpenseItem = (props) => {
+	const [title, setTitle] = useState(props.title);
 	
+	const changeTitle = () => {
+		setTitle('updated');
+	}
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.date } />
 			<div className="expense-item__description">
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
 			</div>
+			<button onClick={changeTitle}>change Title</button>
 		</Card>
 	);
 }
