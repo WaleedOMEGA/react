785b8e4a5f6b087e6a42a77e32a0f0e6de41219b
Waleed OMEGA <waleedomega@hotmail.com>
2022-08-25 01:45:31 +0200

    back after stop

--- styling-component/src/components/CourseGoals/CourseInput/CourseInput.js
+++ styling-component/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,25 +5,36 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
-
+  const [isValid, setIsValid] = useState(true);
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
+		<form onSubmit={formSubmitHandler}>
+			<div className="form-control">
+				<label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+				<input
+					style={{
+						backgroundColor: !isValid ? 'red' : 'black',
+						background: !isValid ? 'salmon' : 'transparent',
+					}}
+					type="text"
+					onChange={goalInputChangeHandler}
+				/>
+			</div>
+			<Button type="submit">Add Goal</Button>
+		</form>
+	);
 };
 
 export default CourseInput;
