442dd8e6c485381e9c4f35028fd88f35e58cbf95
Waleed OMEGA <waleedomega@hotmail.com>
2023-01-04 08:30:39 +0200

    users init

---
+++ users/src/components/Users/AddUser.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Card from "../UI/Card";
+import classes from './AddUser.module.css';
+import Button from "../UI/Button";
+const AddUser=props=>{
+    const addUserHandler = (event) => {
+        event.preventDefault();
+    };
+    return (
+			<Card className={classes.input}>
+				<form onSubmit={addUserHandler}>
+					<label htmlFor="userName">User Name</label>
+					<input id="userName" type="text" />
+					<label htmlFor="age">Age (Years)</label>
+					<input id="age" type="number" />
+					<Button type="submit">Add User</Button>
+				</form>
+			</Card>
+		);
+};
+
+export default AddUser;
