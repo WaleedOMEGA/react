e8f7c5d74ccef7a61d517fd171d2c0613cd4bcc7
Waleed OMEGA <wsabdelaal.c@stc.com.sa>
2023-04-10 10:58:22 +0200

    react memo

--- component-update/src/App.js
+++ component-update/src/App.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
 import Button from './components/UI/Button/Button'
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
   const [showParagraph,setShowParagraph]=useState(false);
@@ -11,7 +12,7 @@ setShowParagraph(prevShowParagraph => !prevShowParagraph);
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>this is new !</p>}
+     <DemoOutput show={showParagraph}/>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph !</Button>
     </div>
   );
