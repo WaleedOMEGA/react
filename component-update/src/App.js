19d029ab8d3702b10c5126df32833c23e578576c
Waleed OMEGA <waleedomega@hotmail.com>
2023-04-13 09:58:05 +0200

    update component complete

--- component-update/src/App.js
+++ component-update/src/App.js
@@ -5,14 +5,23 @@ import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
   const [showParagraph,setShowParagraph]=useState(false);
+  const [allowToggling,setAllowToggling]=useState(false);
 
   const toggleParagraphHandler=useCallback(()=>{
+    if(allowToggling){
 setShowParagraph(prevShowParagraph => !prevShowParagraph);
-  },[]);
+    }
+
+  },[allowToggling]);
+
+  const allowToggleHandler=()=>{
+    setAllowToggling(true);
+  }
   return (
     <div className="app">
       <h1>Hi there!</h1>
      <DemoOutput show={showParagraph}/>
+     <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph !</Button>
     </div>
   );
