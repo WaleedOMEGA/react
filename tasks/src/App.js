e534268fe442818ada3f592b0174268c69cea25c
Waleed OMEGA <wsabdelaal.c@stc.com.sa>
2023-04-19 10:42:42 +0200

    tasks adjust custom hook logic

--- tasks/src/App.js
+++ tasks/src/App.js
@@ -5,23 +5,24 @@ import NewTask from './components/NewTask/NewTask';
 import useHttp from './hooks/use-http';
 function App() {
   const [tasks, setTasks] = useState([]);
-  const transformTasks=tasksObj=>{
-    const loadedTasks = [];
 
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  };
-  const {isLoading,error,sendRequest:fetchTasks}=useHttp({
-    url:'https://react-6d374-default-rtdb.firebaseio.com/tasks.json'
-  },transformTasks);
+  const {isLoading,error,sendRequest:fetchTasks}=useHttp();
 
  
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks=(tasksObj=>{
+      const loadedTasks = [];
+  
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+  
+      setTasks(loadedTasks);
+    });
+    fetchTasks({
+      url:'https://react-6d374-default-rtdb.firebaseio.com/tasks.json'
+    },transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
