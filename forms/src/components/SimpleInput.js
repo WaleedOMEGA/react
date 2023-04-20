a4d9025e66020cf1976ba2a72c264d58b016122f
Waleed OMEGA <waleedomega@hotmail.com>
2023-04-20 11:24:40 +0200

    forms manage of the form validity

--- forms/src/components/SimpleInput.js
+++ forms/src/components/SimpleInput.js
@@ -3,10 +3,17 @@ const SimpleInput = (props) => {
 	const [enteredName, setEnteredName] = useState('');
 
 	const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+	
 
 	const enteredNameIsValid = enteredName.trim() !== '';
 	const nameInputInvalid = !enteredNameIsValid && enteredNameTouched;
 
+	let formIsValid=false;
+
+	
+if(enteredNameIsValid){
+	formIsValid=true;
+}
 	const nameInputChangeHandler = (event) => {
 		setEnteredName(event.target.value);
 	};
@@ -44,7 +51,7 @@ const SimpleInput = (props) => {
 				)}
 			</div>
 			<div className="form-actions">
-				<button>Submit</button>
+				<button disabled={!formIsValid}>Submit</button>
 			</div>
 		</form>
 	);
