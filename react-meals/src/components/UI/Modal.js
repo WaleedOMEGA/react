7bde5988b44c0ea213312448b89a2d423fa5af34
Waleed OMEGA <wsabdelaal.c@stc.com.sa>
2023-05-08 16:00:22 +0300

    react-meals add checkout form

--- react-meals/src/components/UI/Modal.js
+++ react-meals/src/components/UI/Modal.js
@@ -1,7 +1,6 @@
 import { Fragment } from 'react';
-import ReactDOM from 'react-dom/client';
-
 import classes from './Modal.module.css';
+import  ReactDOM  from 'react-dom';
 
 const Backdrop = (props) => {
   return <div className={classes.backdrop} onClick={props.onClose}/>;
@@ -20,6 +19,7 @@ const portalElement = document.getElementById('overlays');
 const Modal = (props) => {
   return (
     <Fragment>
+      
       {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
