5ad5b780d7b553ffdeeee386316b4bf1f44a44c1
Waleed OMEGA <wsabdelaal.c@stc.com.sa>
2023-05-08 14:44:55 +0300

    react meals fetching meals via http

--- react-meals/src/components/UI/Modal.js
+++ react-meals/src/components/UI/Modal.js
@@ -1,32 +1,32 @@
-import classes from './Modal.module.css'
 import { Fragment } from 'react';
+import ReactDOM from 'react-dom/client';
 
-import ReactDOM from 'react-dom';
-const Backdrop=props=>{
-    return (
-<div className={classes.backdrop} onClick={props.onClose}></div>
-    )
+import classes from './Modal.module.css';
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
 
-const ModalOvelay=props=>{
-    return (
-        <div className={classes.modal}>
-             <div className={classes.content}>{props.children}</div>
-        </div>
-       
-            )
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
 };
 
-const portalElement=document.getElementById('overlays')
+const portalElement = document.getElementById('overlays');
 
-const Modal = props=>{
-return (
+const Modal = (props) => {
+  return (
     <Fragment>
-       
-       {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>,portalElement)}
-        {ReactDOM.createPortal(<ModalOvelay>{props.children}</ModalOvelay>,portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
     </Fragment>
-)
+  );
 };
 
 export default Modal;
