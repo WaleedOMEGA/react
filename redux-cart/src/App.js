db0ff3e9460c5d5945c4b089bbc61a14c356be2f
Waleed OMEGA <wsabdelaal.c@stc.com.sa>
2023-05-17 11:48:40 +0300

    using action creator thunk

--- redux-cart/src/App.js
+++ redux-cart/src/App.js
@@ -3,7 +3,7 @@ import { useEffect,Fragment } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { uiActions } from './store/ui-slice';
+import { sendCartData } from './store/cart-slice';
 import Notification from './components/UI/Notification';
 
 let isInitial=true;
@@ -13,41 +13,13 @@ function App() {
 const cart=useSelector((state)=>state.cart);
 const notification=useSelector((state)=>state.ui.notification);
 useEffect(()=>{
-  const sendCartData= async ()=>{
-    dispatch(uiActions.showNotification({
-      status:'pending',
-      title:'sending...',
-      message:'sending cart data',
-    }))
-    const response=await fetch('https://react-6d374-default-rtdb.firebaseio.com/cart.json',{method:'PUT',
-    body:JSON.stringify(cart)
-  });
-
-if(!response.ok){
-  dispatch(uiActions.showNotification({
-    status:'error',
-    title:'Error!',
-    message:'sending cart data failed',
-  }))
-}
-dispatch(uiActions.showNotification({
-  status:'success',
-  title:'success!',
-  message:'Sent cart data Successfully',
-}))
-  };
-
+  
   if(isInitial){
     isInitial=false;
     return;
-  }
- sendCartData().catch(error =>{
-  dispatch(uiActions.showNotification({
-    status:'error',
-    title:'Error!',
-    message:'sending cart data failed',
-  }))
- })
+  };
+  dispatch(sendCartData(cart))
+ 
 },[cart,dispatch])
 
   return (
