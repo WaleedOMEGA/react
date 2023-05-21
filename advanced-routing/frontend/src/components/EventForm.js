ae8c375fd7d59d233d783ad65b4ef5b19ebc3dac
Waleed OMEGA <waleedomega@hotmail.com>
2023-05-22 00:51:58 +0300

    work with action functions

--- advanced-routing/frontend/src/components/EventForm.js
+++ advanced-routing/frontend/src/components/EventForm.js
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
-
+import { Form } from 'react-router-dom';
 function EventForm({ method, event }) {
   const navigate = useNavigate();
   function cancelHandler() {
@@ -9,7 +9,7 @@ function EventForm({ method, event }) {
   }
 
   return (
-    <form className={classes.form}>
+    <Form method='post' className={classes.form}>
       <p>
         <label htmlFor="title">Title</label>
         <input id="title" type="text" name="title" required defaultValue={event ? event.title : ''}/>
@@ -32,7 +32,7 @@ function EventForm({ method, event }) {
         </button>
         <button>Save</button>
       </div>
-    </form>
+    </Form>
   );
 }
 
--- advanced-routing/frontend/src/pages/NewEvent.js
+++ advanced-routing/frontend/src/pages/NewEvent.js
@@ -1,8 +1,30 @@
 import EventForm from "../components/EventForm";
-
+import { json, redirect } from "react-router-dom";
 function NewEventPage(){
 
     return <EventForm />;
 };
 
 export default NewEventPage;
+
+export async function action({request,params}){
+    const data=await request.formData();
+    const eventData={
+        title:data.get('title'),
+        image:data.get('image'),
+        date:data.get('date'),
+        description:data.get('description')
+    }
+    const response=await fetch('http://localhost:8080/events',
+    {method:'POST',
+    headers:{
+        'content-type':'application/json'
+    },    
+body:JSON.stringify(eventData)
+});
+if(!response.ok){
+    throw json({message:'could not save event'},{status:500});
+}
+
+return redirect('/events')
+};
