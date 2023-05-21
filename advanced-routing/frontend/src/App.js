56a88c5798da47b4a84660dc259cda33b6bfc6e5
Waleed OMEGA <waleedomega@hotmail.com>
2023-05-21 12:32:44 +0300

    loader function

--- advanced-routing/frontend/src/App.js
+++ advanced-routing/frontend/src/App.js
@@ -21,7 +21,7 @@
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 import {RouterProvider,createBrowserRouter} from 'react-router-dom'
 import HomePage from './pages/Home';
-import EventsPage from './pages/Events';
+import EventsPage,{loader as eventsLoader} from './pages/Events';
 import EventDetailPage from './pages/EventDetail';
 import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
@@ -32,16 +32,7 @@ const router=createBrowserRouter([
   {path:'/',element:<RootLayout /> ,children:[
     {index:true,element:<HomePage />},
     {path:'events',element:<EventsRootLayout/>,children:[
-      {index:true,element:<EventsPage /> ,loader:async()=>{
-        const response = await fetch('http://localhost:8080/events');
-
-      if (!response.ok) {
-        // ....
-      } else {
-        const resData = await response.json();
-        return resData.events;
-      }
-      }},
+      {index:true,element:<EventsPage /> ,loader:eventsLoader},
       {path:':eventId',element:<EventDetailPage />},
       {path:'new',element:<NewEventPage />},
       {path:':eventId/edit',element:<EditEventPage />}
--- advanced-routing/frontend/src/pages/Events.js
+++ advanced-routing/frontend/src/pages/Events.js
@@ -12,3 +12,15 @@ const events=useLoaderData();
 }
 
 export default EventsPage;
+
+export async function loader(){
+
+    const response = await fetch('http://localhost:8080/events');
+
+      if (!response.ok) {
+        // ....
+      } else {
+        const resData = await response.json();
+        return resData.events;
+      }
+};
