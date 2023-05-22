67632fda8d8864922ab9d404ec716cfb5b6aeae4
Waleed OMEGA <wsabdelaal.c@stc.com.sa>
2023-05-22 15:26:51 +0300

    data fetch with defer

--- advanced-routing/frontend/src/pages/Events.js
+++ advanced-routing/frontend/src/pages/Events.js
@@ -1,33 +1,40 @@
-import { json, useLoaderData } from 'react-router-dom';
+import { json, useLoaderData,defer,Await } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
+import { Suspense } from 'react';
 
 function EventsPage() {
  
-const data=useLoaderData();
-
-// if(data.isError){
-//   return <p>{data.message}</p>
-// }
-const events=data.events;
+const {events}=useLoaderData();
+return (
+  <Suspense fallback={<p style={{textAlign:'center'}}>Loading...</p>}>
+<Await resolve={events}>
+  {(loadedEvents)=><EventsList events={loadedEvents} />}
+</Await>
+  </Suspense>
   
-  return (
-      <EventsList events={events} />
-  );
+)
+
+
+
 }
 
 export default EventsPage;
+async function loadEvents(){
+  const response = await fetch('http://localhost:8080/events');
 
-export async function loader(){
-
-    const response = await fetch('http://localhost:8080/events');
-
-      if (!response.ok) {
-        // return {isError:true,message:'Could not fetch events'}
-        // throw new Response(JSON.stringify({message:'Could not fetch events'}),{status:500});
-        throw json({message:'Could not fetch events'},{status:500})
-      } else {
-        
-        return response;
-      }
+  if (!response.ok) {
+    // return {isError:true,message:'Could not fetch events'}
+    // throw new Response(JSON.stringify({message:'Could not fetch events'}),{status:500});
+    throw json({message:'Could not fetch events'},{status:500})
+  } else {
+    const resData=await response.json();
+    return resData.events;
+  }
+}
+export function loader(){
+return defer({
+  events:loadEvents()
+})
+   
 };
