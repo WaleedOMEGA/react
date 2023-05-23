facb51c3b29eebf3a1a8f4f079f244178c076528
Waleed OMEGA <wsabdelaal.c@stc.com.sa>
2023-05-23 16:42:10 +0300

    manage token expiration

--- authentication/frontend/src/pages/Authentication.js
+++ authentication/frontend/src/pages/Authentication.js
@@ -35,5 +35,8 @@ if(!response.ok){
 const resData= await response.json();
 const token =resData.token;
 localStorage.setItem('token',token);
+const expiration = new Date();
+expiration.setHours(expiration.getHours()+1);
+localStorage.setItem('expiration',expiration.toISOString());
 return redirect('/');
 }
