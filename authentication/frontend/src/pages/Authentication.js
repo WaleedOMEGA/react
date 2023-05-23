d8fc3269641e2e99ee3c43ca38ffeadd859b0ec9
Waleed OMEGA <wsabdelaal.c@stc.com.sa>
2023-05-23 14:34:09 +0300

    attach auth token to outgoing requests

--- authentication/frontend/src/pages/Authentication.js
+++ authentication/frontend/src/pages/Authentication.js
@@ -32,5 +32,8 @@ if(response.status===442 || response.status===401){
 if(!response.ok){
   throw json({message:'Could not authenticate user.'},{status:500})
 }
+const resData= await response.json();
+const token =resData.token;
+localStorage.setItem('token',token);
 return redirect('/');
 }
